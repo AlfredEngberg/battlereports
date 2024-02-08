4741bc8b748c0477191ede428edc59af7f870751
AlfredEngberg <alfred.engberg@elev.ga.ntig.se>
2024-02-08 11:15:22 +0100

    server nere så fixade på sidan

---
+++ dokumentation/dokument.md
@@ -0,0 +1,3 @@
+2024 30/1
+Idag så fixade jag grunden och lade till battlereports i servern
+Jag hade problem med att jag stavade fel på mappen routes men det fixade jag
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', function (req, res) {
-  res.render('index.njk', { title: 'Welcome' })
+  res.render('index.njk', { title: 'Welcome to a site for battlereports on different wargames' })
 })
 
 module.exports = router
