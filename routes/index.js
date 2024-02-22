5bec9cc61dbbe61554e96939a67b2be8aa49a2a4
AlfredEngberg <alfred.engberg@elev.ga.ntig.se>
2024-02-22 11:00:28 +0100

    lägga till data

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 const pool = require('../db')
+const { render } = require('nunjucks')
 
 router.get('/', function (req, res) {
   res.render('index.njk', { title: 'Welcome!' })
@@ -28,7 +29,7 @@ router.get('/dbtest', async function (req, res) {
 
 router.get('/battlereports', async function (req, res) {
 
-  const [battlereports] = await pool.promise().query("SELECT * FROM alfred_battlereports JOIN alfred_spelare ON alfred_spelare.id = alfred_spelare.id")
+  const [battlereports] = await pool.promise().query("SELECT * FROM alfred_battlereports JOIN alfred_spelare ON alfred_battlereports.spelare_id = alfred_spelare.id")
   console.log(battlereports)
   // const part = battlereports.spelare.find((spelare) => spelare.id === 0)
   // res.render('part.njk', {
@@ -60,6 +61,25 @@ router.get('/battlereports/:id', async function (req, res) {
 
   // res.json({battlereports})
   res.render("test.njk", { battlereports} )
+})
+
+router.get('/newplayer', function (req, res){
+  res.render('newplayer.njk', { title: 'Ny spelare' })
+})
+
+router.post('/newplayer', async function (req, res) {
+  console.log(req.body)
+  // plocka ut värden vi ska ha
+  const player = req.body.player
+  const description = req.body.description
+  console.log(breed, description)
+  // säkerhet vad har vi för data
+  // nästa steg är att skriva in i databasen
+  // tableplus för att lära oss SQL fråga
+  // INSERT INTO `alfred_spelare` (`namn1`, `armylist1`, `namn2`, `armylist2`) VALUES
+  // await.pool.promise().query()
+('buffelfisk', 'typ 1 space marine', 'Emrik', '100000 guardsmen');
+  res.json(req.body)
 })
 
 module.exports = router
